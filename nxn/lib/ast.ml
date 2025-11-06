3dc52c2afa152d87d767b429a80e2afedf90d9ff
oxcrow <oxcrow@proton.me>
2025-11-07 00:08:11 +0530

    add: files gitignore and loc,pos to ast
    + Ignore files marked with .ignore prefix.
    + Add loc,pos as recursive nodes to ast
    
    + Sprint.2025.11.B

--- nxn/lib/ast.ml
+++ nxn/lib/ast.ml
@@ -109,3 +109,5 @@ and traits =
 
 and state = ConState | MutState | SetState
 and id = Id of { value : string; loc : loc }
+(* and loc = Loc of { lnum : int; cnum : int } *)
+(* and pos = Pos of { start : loc; end' : loc } *)
